9f3426f738230ce8741afac63e4c414072ae4496
jacerchetoui56 <77737324+jacerchetoui56@users.noreply.github.com>
2022-07-01 16:11:12 +0100

    succeeding making the autoslide

--- src/Slider.js
+++ src/Slider.js
@@ -1,19 +1,26 @@
 import data from './data'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './slider.css'
 
 function Slider() {
   const [people, setPeople] = useState(data)
   const [currentIndex, setIndex] = useState(0)
 
-  // ? we made custom function to make knowing the next and previous index much easier
+  // !=== making sure that the index does not go beyond the length of the array
+  useEffect(() => {
+    const lastIndex = people.length - 1
+    if (currentIndex > lastIndex) setIndex(0)
+    else if (currentIndex < 0) setIndex(lastIndex)
+  }, [currentIndex, people])
 
-  function nextIndex() {
-    return currentIndex === people.length - 1 ? 0 : currentIndex + 1
-  }
-  function previousIndex() {
-    return currentIndex === 0 ? people.length - 1 : currentIndex - 1
-  }
+
+  // ! === making the slider move automatically to the next person every 5 seconds
+
+  useEffect(() => {
+    let interval = setInterval(() => setIndex(currentIndex + 1), 5000)
+    return () => clearInterval(interval)
+  }, [currentIndex])
+
 
   return (
     <main className='main'>
@@ -24,16 +31,19 @@ function Slider() {
         {
           people.map((person, index) => {
             const { name, image, id, title, quote } = person;
+
             // !targetting what is current review and what is the next and the previous
             let className = 'review'
             if (index === currentIndex) {
               className += ' current'
-            } else if (index === previousIndex()) {
+            } else if (index === currentIndex - 1 ||
+              (currentIndex === 0 && index === people.length - 1)) {
               className += ' previous'
-            }
-            else if (index === nextIndex()) {
+            } else if (index === currentIndex + 1 ||
+              (currentIndex === people.length - 1 && index === 0)) {
               className += ' next'
             }
+
             return (
               <div className={className} key={id}>
                 <img src={image} alt={title} className='review-image' />
@@ -49,10 +59,10 @@ function Slider() {
             )
           })
         }
-        <button onClick={() => setIndex(previousIndex)} className='previous-btn'>
+        <button onClick={() => setIndex(currentIndex - 1)} className='previous-btn'>
           <i class="fa-solid fa-chevron-left"></i>
         </button>
-        <button onClick={() => setIndex(nextIndex)} className='next-btn'>
+        <button onClick={() => setIndex(currentIndex + 1)} className='next-btn'>
           <i class="fa-solid fa-chevron-right"></i>
         </button>
       </div>
