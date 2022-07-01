247f9e8d2914e050936ab8ac28fb207532c66ce7
jacerchetoui56 <77737324+jacerchetoui56@users.noreply.github.com>
2022-07-01 15:56:03 +0100

    trying to add auto sliding

--- src/Slider.js
+++ src/Slider.js
@@ -1,12 +1,11 @@
 import data from './data'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './slider.css'
 
 function Slider() {
   const [people, setPeople] = useState(data)
   const [currentIndex, setIndex] = useState(0)
 
-  // ? we made custom function to make knowing the next and previous index much easier
 
   function nextIndex() {
     return currentIndex === people.length - 1 ? 0 : currentIndex + 1
@@ -14,6 +13,8 @@ function Slider() {
   function previousIndex() {
     return currentIndex === 0 ? people.length - 1 : currentIndex - 1
   }
+
+  // !make the slider slide automatically every 4 seconds
 
   return (
     <main className='main'>
